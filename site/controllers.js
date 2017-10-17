09f6be5d6143221fa7accb50fd141699ad79e033
Dario Seminara <dseminara@tbxnet.com>
2017-10-17 14:32:06 -0300

    fixed errors on input data (presets), added axis and angle to UI

--- site/controllers.js
+++ site/controllers.js
@@ -6,13 +6,13 @@ app.controller("MainController", ["$scope", "RotationCalculator", function($scop
   $scope.s2 = [50, 0, 0];
   $scope.p = -1;
 
-  $scope.m1 = [
+  $scope.R1 = [
     [1,0,0],
     [0,1,0],
     [0,0,1]
   ];
 
-  $scope.m2 = [
+  $scope.R2 = [
     [1,0,0],
     [0,1,0],
     [0,0,1]
@@ -21,52 +21,61 @@ app.controller("MainController", ["$scope", "RotationCalculator", function($scop
   $scope.presets = [{
     name: 'Conjunto 1',
     mq: [30, 10, -30],
-    mi: [0.943584990553998, 0.349130937870532],
-    md: [2.97395486662125, 0.308952523348582]
+    m1: [0.943584990553998, 0.349130937870532],
+    m2: [2.97395486662125, 0.308952523348582]
   }, {
     name: 'Conjunto 2',
     mq: [-30, 10, -30],
-    mi: [-1.058854467, 0.3161805526],
-    md: [0.729240437391748, 0.3532833266]
+    m1: [-1.058854467, 0.3161805526],
+    m2: [0.729240437391748, 0.3532833266]
   }, {
     name: 'Conjunto 3',
     mq: [30, -10, -30],
-    mi: [0.9594567146, -0.3014801111],
-    md: [2.88722679, -0.4140686058]
+    m1: [0.9594567146, -0.3014801111],
+    m2: [2.88722679, -0.4140686058]
   },{
     name: 'Conjunto 4',
     mq: [-30, -10, -30],
-    mi: [-1.041762415, -0.3673540994],
-    md: [0.694119083, -0.3271627534]
+    m1: [-1.041762415, -0.3673540994],
+    m2: [0.694119083, -0.3271627534]
   },{
     name: 'Conjunto 5',
     mq: [10, 10, -30],
-    mi: [0.2978972254, 0.338506067],
-    md: [2.193770028, 0.3243603722]
+    m1: [0.2978972254, 0.338506067],
+    m2: [2.193770028, 0.3243603722]
   }];
 
   var updateRotationMatrix = function() {
-    $scope.m1 = RotationCalculator.getRotation({
+    var rotationData;
+    rotationData = RotationCalculator.getRotation({
       Mq: $scope.mq,
-      m: $scope.md,
+      m: $scope.m1,
       s: $scope.s1,
       p: $scope.p
     });
 
-    $scope.m2 = RotationCalculator.getRotation({
+    $scope.angle1 = rotationData.angle;
+    $scope.axis1 = rotationData.axis;
+    $scope.R1 = rotationData.R;
+
+    rotationData = RotationCalculator.getRotation({
       Mq: $scope.mq,
-      m: $scope.mi,
+      m: $scope.m2,
       s: $scope.s2,
       p: $scope.p
     });
+
+    $scope.angle2 = rotationData.angle;
+    $scope.axis2 = rotationData.axis;
+    $scope.R2 = rotationData.R;
   };
 
   $scope.selectedPresetIndex = 0;
   $scope.setPreset = function() {
     $scope.selectedPreset = $scope.presets[$scope.selectedPresetIndex];
     $scope.mq = $scope.selectedPreset.mq;
-    $scope.md = $scope.selectedPreset.md;
-    $scope.mi = $scope.selectedPreset.mi;
+    $scope.m1 = $scope.selectedPreset.m1;
+    $scope.m2 = $scope.selectedPreset.m2;
 
     updateRotationMatrix();
   };
@@ -74,8 +83,8 @@ app.controller("MainController", ["$scope", "RotationCalculator", function($scop
   $scope.setPreset();
 
   $scope.$watch("mq", updateRotationMatrix, true);
-  $scope.$watch("md", updateRotationMatrix, true);
-  $scope.$watch("mi", updateRotationMatrix, true);
+  $scope.$watch("m2", updateRotationMatrix, true);
+  $scope.$watch("m1", updateRotationMatrix, true);
   $scope.$watch("s1", updateRotationMatrix, true);
   $scope.$watch("s2", updateRotationMatrix, true);
 }])
--- site/rotationcalculator.js
+++ site/rotationcalculator.js
@@ -8,7 +8,11 @@ var getRotation = function(params) {
 
   var v0 = MathUtils.vect_add(Mq, s);
   var v1 = [m[0], m[1], -1];
-  return MathUtils.orodrigues_rotation(v0, v1);
+  return {
+  	R: MathUtils.orodrigues_rotation(v0, v1),
+  	angle: Math.asin(MathUtils.vect_sine(v0,v1)),
+  	axis: MathUtils.normalize(MathUtils.vect_product(v0,v1))
+  };
 };
 
 RotationCalculator.getRotation = getRotation;
